6fed40bc4eb720d50669937d5bc60f595bea2005
Jonathan Good <github@bungogood.com>
2023-04-28 23:25:14 +0100

    Adding ignore case

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 use std::error::Error;
-use std::fs;
+use std::{fs};
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-            if args.len() < 3 {
-                return Err("Not enough arguments");
-            }
-    
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-    
-            Ok(Config { query, file_path })
+        if args.len() < 3 {
+            return Err("Not enough arguments");
         }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        let ignore_case = true;
+
+        Ok(Config { query, file_path, ignore_case })
+    }
+}
+
+pub fn search_caseless<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    search(&query, contents)
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -32,6 +40,12 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_caseless(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
 
     for line in search(&config.query, &contents) {
         println!("{line}");
