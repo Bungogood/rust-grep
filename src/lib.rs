27c0a6141f19c54eff88d2b03c57f912fe6d6a5e
Jonathan Good <github@bungogood.com>
2023-04-28 23:16:01 +0100

    Filtering lines

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,23 @@ impl Config {
         }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = vec![];
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    print!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
