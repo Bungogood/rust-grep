5760c97649cd861a2a8449f123354ac985de7cd1
Jonathan Good <github@bungogood.com>
2023-04-28 23:08:02 +0100

    Moving to libary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::process;
+use std::env;
+use rg::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    if let Err(e) = rg::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
